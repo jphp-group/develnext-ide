427038482b1c0a76aa8717d3f1b40c7fd32f05f3
Dmitriy Zayceff <d.zayceff@gmail.com>
2018-05-12 13:44:03 +0300

    upd.

--- package.build.php
+++ package.build.php
@@ -1,1 +1,15 @@
+<?php
+use packager\Event;
 
+function task_publish(Event $e)
+{
+    Tasks::runExternal('./dn-app-framework', 'publish', [], ...$e->flags());
+}
+
+/**
+ * @jppm-task hub:publish
+ */
+function task_hubPublish(Event $e)
+{
+    Tasks::runExternal('./dn-app-framework', 'hub:publish', [], ...$e->flags());
+}
