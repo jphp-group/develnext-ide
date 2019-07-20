4824b85a2718b1d70a8295342f7d17e19cdb1dba
Ts.Saltan <TsSaltan@gmail.com>
2019-07-20 13:50:00 +0300

    Theme changing in settings fotm

--- ide/src/ide/forms/SettingsForm.php
+++ ide/src/ide/forms/SettingsForm.php
@@ -4,34 +4,48 @@ namespace ide\forms;
 use ide\Ide;
 use ide\IdeLanguage;
 use ide\Logger;
+use ide\commands\ChangeThemeCommand;
 use ide\forms\AbstractIdeForm;
+use php\gui\UXComboBox;
 use php\gui\UXImage;
 use php\gui\UXLabel;
 use php\gui\UXListCell;
 use php\gui\paint\UXColor;
 use php\lang\JavaException;
+use php\lib\str;
 
 
 /**
  * @property UXLabel $splashLabel
  * @property UXCheckBox $splashShowCheck
  * @property UXLabel $langLabel
- * @property UXCombobox $langCombo
+ * @property UXComboBox $langCombo
  * @property UXLabel $versionLabel
  * @property UXButton $saveButton
+ * @property UXLabel $themeLabel
+ * @property UXComboBox $themeCombo
  */
 class SettingsForm extends AbstractIdeForm
 {	
+    /**
+     * @var ChangeThemeCommand
+     */
+    protected $themeCommand;
+
     /**
      * @event show
      */
     public function doShow()
     {
         Logger::info("Show settings form ...");
+
+        $this->themeCommand = Ide::get()->getRegisteredCommand(ChangeThemeCommand::class);
         $this->loadConfigs();
 
     	$this->langCombo->onButtonRender([$this, 'uiLanguageRender']); // Отрисовка выбранного элемента
-    	$this->langCombo->onCellRender([$this, 'uiLanguageRender']); // Отрисовка выпадающего списка
+    	$this->langCombo->onCellRender([$this, 'uiLanguageRender']); // Отрисовка выпадающего списка     
+        $this->themeCombo->onButtonRender([$this, 'uiThemeRender']); // Отрисовка выбранного элемента
+        $this->themeCombo->onCellRender([$this, 'uiThemeRender']); // Отрисовка выпадающего списка
         $this->saveButton->on('action', [$this, 'doSave']);
     }
 
@@ -53,6 +67,7 @@ class SettingsForm extends AbstractIdeForm
     	// Загрузка списка языков
     	$currentLang = Ide::get()->getUserConfigValue('ide.language');
     	$languages = Ide::get()->getLanguages();
+        $this->langCombo->items->clear();
     	$index = 0;
     	foreach ($languages as $code => $language) {
     		/** @var IdeLanguage $language **/
@@ -63,6 +78,13 @@ class SettingsForm extends AbstractIdeForm
             }
             $index++;
     	}
+
+        // Загрузка тем
+        $themes = $this->themeCommand->getThemes();
+        $currentTheme = $this->themeCommand->getCurrentTheme();
+        $this->themeCombo->items->clear();
+        $this->themeCombo->items->addAll($themes);
+        $this->themeCombo->selectedIndex = array_search($currentTheme, $themes);
     }
 
     /**
@@ -86,19 +108,42 @@ class SettingsForm extends AbstractIdeForm
     }
 
     /**
+     * Пусть названия тем будут с большой буквы
+     * @param  UXListCell $item  Ячейка в comboBox
+     * @param  string     $value Значение ячейки
+     */
+    public function uiThemeRender(UXListCell $item, $value){
+        $item->text = str::upperFirst($value);
+    }
+
+    /**
      * Сохранение параметров и закрытие формы
      */
     public function doSave(){
-    	$this->hide();
-
     	// Параметры сплеша
     	Ide::get()->setUserConfigValue('ide.splash', $this->splashShowCheck->selected ? 1 : 0);
 
-    	// Параметры языка
-    	$langCode = $this->langCombo->selected;
-    	if(isset(Ide::get()->getLanguages()[$langCode])){
-    		Ide::get()->setUserConfigValue('ide.language', $langCode);
-        	Ide::get()->getLocalizer()->language = $langCode;
-    	}
+
+        // Применение темы
+        //uiLater(function(){
+
+        $theme = $this->themeCombo->selected;
+        $themeIndex = $this->themeCombo->selectedIndex;
+        $this->themeCommand->setCurrentTheme($theme);
+        $this->themeCommand->onExecute();
+
+        // Параметры языка
+        $langCode = $this->langCombo->selected;
+        if(isset(Ide::get()->getLanguages()[$langCode])){
+            Ide::get()->setUserConfigValue('ide.language', $langCode);
+            Ide::get()->getLocalizer()->language = $langCode;
+        }
+
+        //});
+
+        /*waitAsync(1000, function(){
+            $this->hide();
+        });*/
+        $this->loadConfigs();
     }
 }
