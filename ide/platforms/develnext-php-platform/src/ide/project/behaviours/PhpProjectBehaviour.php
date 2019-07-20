e862f0afaad77c3c1247774bc523637c980b6acc
TsSaltan <TsSaltan@users.noreply.github.com>
2019-07-20 12:25:22 +0300

    [Bugfix] Метод импортирования классов (#8)

--- ide/platforms/develnext-php-platform/src/ide/project/behaviours/PhpProjectBehaviour.php
+++ ide/platforms/develnext-php-platform/src/ide/project/behaviours/PhpProjectBehaviour.php
@@ -1,24 +1,24 @@
 <?php
 namespace ide\project\behaviours;
 
-use develnext\lexer\inspector\PHPInspector;
 use Error;
-use ide\formats\PhpCodeFormat;
+use develnext\lexer\inspector\PHPInspector;
 use ide\Logger;
+use ide\formats\PhpCodeFormat;
 use ide\project\AbstractProjectBehaviour;
+use ide\project\Project;
+use ide\project\ProjectFile;
 use ide\project\behaviours\php\TreeCreatePhpClassMenuCommand;
 use ide\project\behaviours\php\TreeCreatePhpFileMenuCommand;
 use ide\project\control\CommonProjectControlPane;
-use ide\project\Project;
-use ide\project\ProjectFile;
 use ide\utils\FileUtils;
 use php\compress\ZipFile;
 use php\framework\FrameworkPackageLoader;
-use php\gui\layout\UXHBox;
-use php\gui\layout\UXVBox;
 use php\gui\UXCheckbox;
 use php\gui\UXComboBox;
 use php\gui\UXLabel;
+use php\gui\layout\UXHBox;
+use php\gui\layout\UXVBox;
 use php\io\File;
 use php\io\FileStream;
 use php\io\IOException;
@@ -32,6 +32,7 @@ use php\lib\arr;
 use php\lib\fs;
 use php\lib\str;
 use php\net\URL;
+use php\util\Flow;
 
 /**
  * Class PhpProjectBehaviour
@@ -117,9 +118,20 @@ class PhpProjectBehaviour extends AbstractProjectBehaviour
         $this->registerTreeMenu();
     }
 
-    public function getImportType()
-    {
-        return $this->getIdeConfigValue(self::OPT_IMPORT_TYPE_CODE, 'simple');
+    /**
+     * Получить метод импортирования классов
+     * @return  string simple|package
+     */
+    public function getImportType(): string
+    {   
+        $default = 'simple';
+        $type = $this->getIdeConfigValue(self::OPT_IMPORT_TYPE_CODE, $default);
+        if(!isset(static::$importTypes[$type])){
+            $this->setImportType($default);
+            return $default;
+        }
+
+        return $type;
     }
 
     public function setImportType($value)
@@ -527,7 +539,7 @@ class PhpProjectBehaviour extends AbstractProjectBehaviour
     {
         if ($this->uiSettings) {
             $this->uiByteCodeCheckbox->selected = $this->getIdeConfigValue(self::OPT_COMPILE_BYTE_CODE, true);
-            $this->uiImportTypesSelect->value   = _(static::$importTypes[$this->getImportType()]);
+            $this->uiImportTypesSelect->selectedIndex = Flow::of(arr::keys(static::$importTypes))->findValue($this->getImportType());
         }
     }
 
@@ -550,9 +562,7 @@ class PhpProjectBehaviour extends AbstractProjectBehaviour
         $importTypeSelect = new UXComboBox(static::$importTypes);
 
         $importTypeSelect->on('action', function () {
-            uiLater(function () {
-                $this->setImportType(arr::keys(static::$importTypes)[$this->uiImportTypesSelect->selectedIndex]);
-            });
+            $this->setImportType(arr::keys(static::$importTypes)[$this->uiImportTypesSelect->selectedIndex]);
         });
 
         $this->uiImportTypesSelect = _($importTypeSelect);
