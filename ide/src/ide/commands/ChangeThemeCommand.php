8bfdaf11e484c3656061eec389b04d56292bf435
Ts.Saltan <TsSaltan@gmail.com>
2019-07-20 13:50:00 +0300

    Theme changing in settings fotm

--- ide/src/ide/commands/ChangeThemeCommand.php
+++ ide/src/ide/commands/ChangeThemeCommand.php
@@ -11,32 +11,72 @@ use php\gui\framework\FormCollection;
 use php\lib\str;
 
 class ChangeThemeCommand extends AbstractCommand {
-    public static $instance;
-
+    /**
+     * Предыдущая используемая тема
+     * При добавлении новой темы сначала удаляется старая
+     * @var string|null
+     */
     protected $prevTheme = null;
+
+    /**
+     * Текущая используемая тема
+     * @var string|null
+     */
     protected $currentTheme = null;
+
+    /**
+     * Список доступных тем
+     * Темы хранятся по пути $themePath
+     * По дефолту берётся первая тема из этого массива
+     * @var string[]
+     */
+    protected $themes = ['light', 'dark'];
+
+    /**
+     * Путь к css файлам с темами
+     * Начинается с директории /dn-app-framework/src/
+     * @var string
+     */
+    protected $themePath = '/php/gui/framework/styles/';
 
     public function __construct() {
         parent::__construct();
-        self::$instance = $this;
         FormCollection::onAddEvent([$this, 'applyStylesheet']);
+        $this->prevTheme = $this->getCurrentTheme();
     }
 
-    public function getCurrentTheme(){
+    /**
+     * Получить текущую используемую в среде тему
+     * @return string
+     */
+    public function getCurrentTheme(): string {
+        $default = $this->themes[0];
         if(is_null($this->currentTheme)){
-            $this->currentTheme = Ide::get()->getUserConfigValue('ide.theme', 'light');
+            $this->currentTheme = Ide::get()->getUserConfigValue('ide.theme', $default);
         }
 
-        return $this->currentTheme;
+        // Проверка, существует ли тема
+        return in_array($this->currentTheme, $this->themes) ? $this->currentTheme : ($this->currentTheme = $default);
     }
 
+    /**
+     * Установить тему для ide (для применения нужно вызвать метод onExecute)
+     * @param string $theme Имя темы (без пути и без расширения)
+     */
     public function setCurrentTheme(string $theme){
+        // Проверка, существует ли тема
+        $theme = (in_array($theme, $this->themes)) ? $theme : $this->themes[0];
         Ide::get()->setUserConfigValue('ide.theme', $theme);
         $this->currentTheme = $theme;
+
+        Logger::info('Set IDE theme: ' . $theme);
     }
 
-    public function getName()
-    {
+    public function getThemes(): array {
+        return $this->themes;
+    }
+
+    public function getName(){
         return 'theme.changer';
     }
 
@@ -54,13 +94,13 @@ class ChangeThemeCommand extends AbstractCommand {
 
     public function applyStylesheet(AbstractForm $form){
         if(str::length($this->prevTheme) > 0){
-            $prev = '/php/gui/framework/styles/' . $this->prevTheme . '.css';
-            Logger::debug('Stylesheet ' . $prev . ' removed from ' . $form->getName());
+            $prev = $this->themePath . $this->prevTheme . '.css';
+            Logger::info('Stylesheet ' . $prev . ' removed from ' . $form->getName());
             $form->removeStylesheet($prev);
         }
 
-        $current = '/php/gui/framework/styles/' . $this->getCurrentTheme() . '.css';
-        Logger::debug('Stylesheet ' . $current . ' applied to ' . $form->getName());
+        $current = $this->themePath . $this->getCurrentTheme() . '.css';
+        Logger::info('Stylesheet ' . $current . ' applied to ' . $form->getName());
         $form->addStylesheet($current);
     }
 }
